30a1996676692324571fd7971ddb52357f685a4b
gbarnes <fx-designer@web.de>
2018-01-27 21:45:46 +0100

    added distance to raycast (5meters) other minor changes

--- Game/GGJ18_Game01/Assets/Scripts/Game/Network/Player.cs
+++ Game/GGJ18_Game01/Assets/Scripts/Game/Network/Player.cs
@@ -157,7 +157,7 @@ public class Player : NetworkBehaviour
     {
         RaycastHit hit;
         //Ray forwardRay = new Ray(Camera.main.transform.position, transform.forward);
-        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, Mathf.Infinity, LayerMask.GetMask("Interaction")))
+        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, 5.0f, LayerMask.GetMask("Interaction")))
         {
             InteractableObject tempObj = hit.transform.gameObject.GetComponent<InteractableObject>();            
             
