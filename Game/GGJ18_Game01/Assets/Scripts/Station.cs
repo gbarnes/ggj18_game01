514da194d16cbf2e0187c0eab4ff04576728b9ca
gbarnes <fx-designer@web.de>
2018-01-27 09:29:32 +0100

    added network functionality to stations

--- Game/GGJ18_Game01/Assets/Scripts/Station.cs
+++ Game/GGJ18_Game01/Assets/Scripts/Station.cs
@@ -6,14 +6,37 @@ using UnityEngine.Networking;
 public class Station : InteractableObject
 {
     public NetworkInstanceId OwnerId;
+    [SyncVar]
+    public int crystals = 0;
+    public bool isBlueStation = true;
 
     public void AccessByPlayer(Player p)
     {
+        if(OwnerId == p.netId && p.holdingItem != ItemType.None)
+        {
+            if (isBlueStation && p.holdingItem != ItemType.Crystal_Blue)
+                return;
+            else if (!isBlueStation && p.holdingItem != ItemType.Crystal_Red)
+                return;
+            
+            crystals++;
+            p.holdingItem = ItemType.None;
 
+            // WON!
+            if (crystals == 3)
+            {
+                return;
+            }
+        }
+        else if(p.holdingItem == ItemType.None && crystals > 0)
+        {
+            crystals--;
+            p.holdingItem = (isBlueStation) ? ItemType.Crystal_Blue : ItemType.Crystal_Red;
+            return;
+        }
     }
 
-
-    public bool isBlueStation = true;
+    
     public ItemType crystal1;
     public ItemType crystal2;
     public ItemType crystal3;
