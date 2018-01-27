4cf754c3e6d8d579eeb6a418fa4c0e79e5ecfa46
gbarnes <fx-designer@web.de>
2018-01-27 04:46:14 +0100

    Interaction with Dummyobject

---
+++ Game/GGJ18_Game01/Assets/Scripts/Stash.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ItemType
+{
+    None,
+    Antenna_Red,
+    Chip_Red,
+    Fuse_Red,
+    Antenna_Blue,
+    Chip_Blue,
+    Fuse_Blue
+}
+
+public class Stash : MonoBehaviour {
+    private ItemType item;
+
+    public ItemType Item
+    {
+        get { return item; }
+        set { item = value; }
+    }
+
+	// Use this for initialization
+	void Start () {
+        item = ItemType.Antenna_Blue;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
