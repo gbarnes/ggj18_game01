239e636d7675a7ea0dbb69f3defecb4c1bae3b96
IXander89 <a.romanjuk@hotmail.com>
2018-01-27 02:16:39 +0100

    spherical gravity movement

---
+++ Game/GGJ18_Game01/Assets/Scripts/Game/Gameplay/PlayerController.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float Gravity = 1;
+    public float Jump = 1;
+    public float Accleration = 1;
+    public float MaxSpeed = 10;
+    public float SensitivityX = 10;
+    public float SensitivityY = 10;
+    public float ClampMin = -300;
+    public float ClampMax = 300;
+    public Camera Cam;
+    public float Angle;
+    private Rigidbody _rig;
+    private Vector3 _gravity;
+
+    void Start()
+    {
+        _rig = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        ApplyMovement();
+        _gravity = -this.transform.position.normalized * Gravity;
+        _rig.velocity += _gravity;
+        transform.rotation = Quaternion.FromToRotation(transform.up, -_gravity) * transform.rotation;
+    }
+
+    private void ApplyMovement()
+    {
+        float rot = Input.GetAxis("Mouse Y");
+        Quaternion yRot = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * SensitivityY, Vector3.up);
+
+        this.transform.localRotation *= yRot;
+        Angle = Vector3.SignedAngle(transform.forward, Cam.transform.forward, -transform.right);
+
+        if ((Angle > ClampMin && rot < 0) || (Angle < ClampMax && rot > 0))
+        {
+            Quaternion xRot = Quaternion.AngleAxis(rot * SensitivityX, Vector3.left);
+            Cam.transform.localRotation *= xRot;
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            this._rig.velocity -= _gravity * Jump;
+            //return;
+        }
+
+        var currentSpeed = _rig.velocity;
+
+        Vector3 speed = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            speed += this.transform.forward * Accleration;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            speed += -this.transform.forward * Accleration;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            speed += this.transform.right * Accleration;
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            speed += -this.transform.right * Accleration;
+        }
+        if (this._rig.velocity.magnitude < MaxSpeed)
+            this._rig.velocity += speed;
+    }
+}
---
+++ Game/GGJ18_Game01/Assets/Scripts/Game/Gameplay/SphericalGravity.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphericalGravity : MonoBehaviour
+{
+    public GameObject Planet;
+    public GameObject Player;
+
+    public int Count;
+    public float Radius;
+
+    public float Min = 1;
+    public float Max = 1;
+    public float YMult = 1;
+
+    private GameObject _environment;
+    // Use this for initialization
+    void Start()
+    {
+        Planet.transform.localScale = Vector3.one * Radius / 2;
+        Player.transform.position = Vector3.up * (Radius + 2);
+        _environment = new GameObject("Environment");
+        Spawn();
+
+        if (Planet == null)
+        {
+            Debug.Log("Missing GravityCenter Object!");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direction = Planet.transform.position - Player.transform.position;
+        Physics.gravity = direction;
+    }
+
+    private void Spawn()
+    {
+        for (int i = 0; i < Count; i++)
+        {
+            Vector3 dir = Random.onUnitSphere * Radius*0.5f*1.2f;
+            RaycastHit hitInfo;
+            if (Physics.Raycast(dir, -dir, out hitInfo))
+            {
+                int value = Random.Range(0,3);
+                GameObject obj;
+                obj = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                switch (value)
+                {
+                    case 0:
+                        obj = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                        break;
+                    case 1:
+                        obj = GameObject.CreatePrimitive(PrimitiveType.Capsule);
+                        break;
+                    case 2:
+                        obj = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+                        break;
+                    case 3:
+                        obj = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+                        break;
+                }
+                
+                obj.transform.position = hitInfo.point;
+                obj.transform.rotation = Quaternion.FromToRotation(obj.transform.up, dir) * obj.transform.rotation;
+                obj.transform.SetParent(_environment.transform);
+                obj.transform.localScale = new Vector3(Random.Range(Min, Max), Random.Range(Min, Max)* YMult, Random.Range(Min, Max));
+            }
+        }
+    }
+}
