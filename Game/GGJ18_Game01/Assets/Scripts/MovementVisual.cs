541062877df1d9013a54cd7ee598cd1e60eb72ea
gbarnes <fx-designer@web.de>
2018-01-28 08:35:00 +0100

    crystals now show their correct material

--- Game/GGJ18_Game01/Assets/Scripts/MovementVisual.cs
+++ Game/GGJ18_Game01/Assets/Scripts/MovementVisual.cs
@@ -3,7 +3,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MovementVisual : MonoBehaviour {
+public class MovementVisual : MonoBehaviour
+{
     
     public float localX = 0.373f, localY = -0.214f, localZ = 0.416f;
     public Vector3 crystalPos = new Vector3(0.373f, -0.214f, 0.416f);
@@ -11,17 +12,21 @@ public class MovementVisual : MonoBehaviour {
     public bool animationRunning = false;
     public float animationSpeed = 0.01f;
     public float animationStep = 0.0f;
+    public Material RedMaterial;
+    public Material BlueMaterial;
     public ItemType olditem = ItemType.None;
 
     private Player _player;
 	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
         nullPos = new Vector3(0.33f, -0.565f, 0.519f);
         _player = Locator.Get<Player>();
     }
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
         if(_player == null)
             _player = Locator.Get<Player>();
 
@@ -84,30 +89,25 @@ public class MovementVisual : MonoBehaviour {
         if (item == ItemType.Crystal_Blue && item != olditem && item != ItemType.Crystal_Red)
         {
             olditem = item;
-           // GetComponent<Renderer>().material.SetColor("_Color", new Color(0, 0, 1));
+            this.GetComponentInChildren<Renderer>().material = BlueMaterial;
             animationRunning = true;
         }
-    else if(item == ItemType.Crystal_Red && item != olditem && item != ItemType.Crystal_Blue)
+        else if(item == ItemType.Crystal_Red && item != olditem && item != ItemType.Crystal_Blue)
         {
             olditem = item;
-           // GetComponent<Renderer>().material.SetColor("_Color", new Color(1, 0, 0));
+            this.GetComponentInChildren<Renderer>().material= RedMaterial;
             animationRunning = true;
         }
-    else if (item == ItemType.None && item != olditem)
+        else if (item == ItemType.None && item != olditem)
         {
             olditem = item;
             animationRunning = true;
          
         }
-       
-        
     }
 
     void hideCrystal()
     {
         
     }
-
-
-   
 }
